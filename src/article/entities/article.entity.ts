aa0b028c519c214d9bd30058d2fe340a48a5fd25
Daniel DOVI <daniel.doviakon@gmail.com>
2023-03-06 01:41:04 +0000

    ms a jour backend pour articles

---
+++ src/article/entities/article.entity.ts
@@ -0,0 +1,43 @@
+import {
+    Entity,
+    Column,
+    CreateDateColumn,
+    UpdateDateColumn,
+    PrimaryGeneratedColumn,
+    ManyToOne,
+    JoinColumn
+} from 'typeorm'
+import {Users} from "../../user/entities/user.entity";
+
+@Entity()
+export class Article {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    title: string
+
+    @Column({ default: 1 })
+    active: number
+
+    @Column({ nullable: true })
+    category: string
+
+    @Column({ nullable: true })
+    description: string
+
+    @Column({ nullable: true })
+    image: string
+
+    @ManyToOne(() =>
+            Users, user => user.id,
+        { onDelete: 'SET NULL' })
+    @JoinColumn()
+    writer: Users;
+
+    @CreateDateColumn()
+    createdAt: String
+
+    @UpdateDateColumn()
+    updtedAt: String
+}
